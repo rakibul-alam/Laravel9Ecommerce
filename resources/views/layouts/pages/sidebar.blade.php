d029784b94e02c27571a65caa7b0b773f4713676
unknown <rakibul1295@gmail.com>
2022-12-06 18:18:46 +0600

    add

--- resources/views/layouts/pages/sidebar.blade.php
+++ resources/views/layouts/pages/sidebar.blade.php
@@ -1,4 +1,4 @@
-    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <aside class="main-sidebar sidebar-dark-primary elevation-4" style="overflow-y: scroll;">
         <!-- Brand Logo -->
         <a href="index3.html" class="brand-link">
             <img src="dist/img/AdminLTELogo.png" class="brand-image img-circle elevation-3" style="opacity: .8">
@@ -10,10 +10,11 @@
             <!-- Sidebar user panel (optional) -->
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2">
+                    <img src="https://w3schools.com/w3images/avatar5.png" class="img-circle elevation-2">
+
                 </div>
                 <div class="info">
-                    <a href="#" class="d-block">Alexander Pierce</a>
+                    <a href="#" class="d-block">{{ Auth::user()->name }}</a>
                 </div>
             </div>
 
